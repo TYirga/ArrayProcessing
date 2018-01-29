36423d44d7ca83049653191c6df05396e47fd6d1
tizazu <esyan4>
2018-01-29 13:57:57 -0500

    Array_Max is added

--- src/main/java/tize/com/example/demo/ArraySum.java
+++ src/main/java/tize/com/example/demo/ArraySum.java
@@ -7,17 +7,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ArraySum {
     @RequestMapping("/")
-    public String ArrayProcessing(){
-      Integer [] arraySum={4,16,64,128};
+    public String ArrayProcessing(@RequestParam("size") int size){
+     Integer [] arraySum={4,16,64,128};
+
         int sum=0;
         double average=0;
-      for(int i=0;i<arraySum.length;i++){
+      for(int i=0;i<size;i++){
 
         sum+=arraySum[i];
-        average=sum/(arraySum.length);
-        }
+        average=sum/(arraySum.length);}
 
-return " Sum=" +sum +"</br/>" + "Average= "+ average;
+        //Find maximum in the Array
+          int maxArray=arraySum[0];
+      for(int i=1;i<arraySum.length;i++){
+          if(arraySum[i]>maxArray)
+              maxArray=arraySum[i];
+      }
+
+return " Sum=" +sum +"<br/>" + "Average= "+ average + "<br/> Maximum in the array = "+ maxArray;
 
     }
 
